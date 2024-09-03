3a8ef43d0a793433e0344112d2fd198bf70684e9
Siddharth Bajpai <139347604+siddharthbajpai@users.noreply.github.com>
2024-09-03 13:47:25 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,11 +2,16 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"  # or whatever version you're using
+      version = "~> 3.0"  # or your preferred version
     }
   }
 }
 
 provider "aws" {
-  region = "us-west-2"  # or your desired region
+  region = "us-west-2"
+
+  assume_role {
+    role_arn = "arn:aws:iam::471112789726:role/terraform-role"
+  }
 }
+
