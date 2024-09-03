4b252637ef29da2f1b6e4d9d0fdca83a81068026
Siddharth Bajpai <139347604+siddharthbajpai@users.noreply.github.com>
2024-09-03 15:22:26 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region  = "us-west-2"
   assume_role {
-    role_arn = "arn:aws:iam::471112789726:instance-profile/ecr-role"
+    role_arn = "arn:aws:iam::471112789726:role/ecr-role"
   }
 }
