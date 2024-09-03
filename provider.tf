2bdb637f624f348da3cd231ff2babdf1338cd92d
Siddharth Bajpai <139347604+siddharthbajpai@users.noreply.github.com>
2024-09-03 15:19:15 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,20 +1,6 @@
-terraform {
-  required_version = ">= 0.13.5"
-}
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
 provider "aws" {
-  region = "us-west-2"
-
+  region  = "us-west-2"
   assume_role {
-    role_arn = "arn:aws:iam::471112789726:role/terraform-role"
+    role_arn = "arn:aws:iam::471112789726:instance-profile/ecr-role"
   }
 }
